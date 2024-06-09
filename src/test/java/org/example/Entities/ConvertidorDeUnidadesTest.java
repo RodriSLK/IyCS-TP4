0a054c00f9e03671bf8a408b7d1c3ccd0241fcf8
PauRodriguezz <paurodriguez028@gmail.com>
2024-06-08 23:49:13 -0300

    Creacion carpeta test y test unitarios

---
+++ src/test/java/org/example/Entities/ConvertidorDeUnidadesTest.java
@@ -0,0 +1,28 @@
+package org.example.Entities;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConvertidorDeUnidadesTest {
+    private final ConvertidorDeUnidades convertidor = new ConvertidorDeUnidades();
+
+    //Test Pau
+    @Test
+    public void pruebaMillaAMetro_valorPositivo() {
+        assertEquals(1609.34, convertidor.millaAMetro(1), 0.0001);
+    }
+
+    @Test
+    public void pruebaMillaAMetro_cero() {
+        assertEquals(0, convertidor.millaAMetro(0), 0.0001);
+    }
+
+    @Test
+    public void pruebaMillaAMetro_valorNegativo() {
+        assertEquals(-1609.34, convertidor.millaAMetro(-1), 0.0001);
+    }
+
+
+}
